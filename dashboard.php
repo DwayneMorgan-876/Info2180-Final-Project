c7074a6cc56c6e822707649db7b9391ca32cd8c6
Ashni-Nay Wilson <ashninaywilson@yahoo.com>
2020-12-05 15:57:09 -0500

    Added php

--- dashboard.php
+++ dashboard.php
@@ -3,7 +3,6 @@
 require_once 'password.php';
 session_start();
 
-// if (isset($_SESSION['id'])) {
     try {
         $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8mb4", $username, $password);
         $stmt = $pdo->query("SELECT * FROM Issues");
@@ -13,6 +12,4 @@ session_start();
     } catch (Exception $e) {
         alert($e->getMessage());
     }
-// } else {
-//     header("Location:./login.html");
-// }
+
