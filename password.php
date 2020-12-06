d78032df3808f51002cac06883f05f38d6c3eee7
Anjinari Stephenson <aastephenson@atlassian.com>
2020-12-06 09:45:05 -0500

    Edit

--- password.php
+++ password.php
@@ -2,26 +2,7 @@
 
 $host = 'localhost';
 $db = 'schema';
-<<<<<<< HEAD
 $username = 'admin@project2.com';
-$password = 'password123';
-
-// function validatePassword($pword) {
-//     if (strlen($pword) > 8) {
-//         if(preg_match('/[0-9+]/', $pword)) {
-//             return $pword;
-//         } else {
-//             echo "Invalid password criteria\n";
-//         }
-//     } else {
-//         echo "Invalid password length\n";
-//         var_dump($pword);
-//     }
-// }
-
-?>
-=======
-$username = 'root';
 $password = 'password123';
 
 function validatePassword($pword) {
@@ -29,11 +10,12 @@ function validatePassword($pword) {
         if(preg_match('/[0-9+]/', $pword)) {
             return $pword;
         } else {
-            echo "Your password contains invalid characaters\n";
+            echo "Invalid password criteria\n";
         }
     } else {
-        echo "Your password is too short\n";
+        echo "Invalid password length\n";
         var_dump($pword);
     }
 }
->>>>>>> ccdc3e1ade8fed16a19c231204e47779c071c640
+
+?>
